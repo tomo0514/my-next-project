c02a26da44afffd9a933e89e870982c93475fb91
tomo0514 <ktc24a31c0007@edu.kyoto-tech.ac.jp>
2024-11-30 16:35:03 +0900

    次は73ページから

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,72 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-export default function Home() {
-  // javaScriptの定数や式などを書くことができる
-  const name = "世界";
+import NewsList from "@/app/components/NewsList";
+import ButtonLink from "@/app/components/ButtonLink";
+import{News} from"@/app/libs/microcms";
 
-  // HTMLのようなマークアップを書くことができる
-  // JavaScriptを書いたり参照する際には{}で囲む
+const data: {
+  contents:News[] } = {
+    contents: [
+      {
+        id: "1",
+        title:
+"渋谷にオフィスを移転しました",
+        category: {
+          name:"更新情報",
+        },
+        publishedAt: "2023/05/19",
+        createdAt: "2023/05/19",
+      },
+      {
+        id: "2",
+        title: "当社CEDが業界リーダーTOP30に選出されました",
+        category: {
+          name: "更新情報",
+        },
+        publishedAt: "2023/05/19",
+        createdAt: "2023/05/19",
+      },
+      {
+        id: "3",
+        title:"テストの記事です",
+        category:{
+          name:"更新情報",
+        },
+        publishedAt:"2023/04/19",
+        createdAt: "2023/04/19",
+      },
+    ],
+  };
+
+export default function Home() {
+  const sliceData = data.contents.slice(0,2);
+  //const sliceData: News = [];
+
   return (
-    <section className={styles.top}>
-      <div>
-        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-        <p className={styles.description}>
-          私たちは市場をリードしているグローバルテックカンパにーです。
-        </p>
-      </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-    </section>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパにーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news} >
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
   );
 }
